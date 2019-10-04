8ccdaf438f5686fb0d71da7c36e9988124c79939
Angie Xiaotong Huang <angie.granberg@gmail.com>
2019-10-04 14:19:22 +0200

    Updates

---
+++ precache-manifest.6258650a0c5b7bed0fd7fe5f38e4de32.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7a5233408c64fc5dab8be91ad3da70",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "92bc4f10cff0390c73ee",
+    "url": "/myapp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "459dabb081d4fac6c96e",
+    "url": "/myapp/static/css/main.dc25f549.chunk.css"
+  },
+  {
+    "revision": "92bc4f10cff0390c73ee",
+    "url": "/myapp/static/js/2.8175b68a.chunk.js"
+  },
+  {
+    "revision": "459dabb081d4fac6c96e",
+    "url": "/myapp/static/js/main.1771977a.chunk.js"
+  },
+  {
+    "revision": "4e5748567161e2987eb3",
+    "url": "/myapp/static/js/runtime-main.4d6b0137.js"
+  },
+  {
+    "revision": "d97b0c340f3a26da09ca9aa860ac53a5",
+    "url": "/myapp/static/media/angiecv.d97b0c34.pdf"
+  },
+  {
+    "revision": "e95329dabc8f38078f880946e1e26fe7",
+    "url": "/myapp/static/media/coding1.e95329da.png"
+  },
+  {
+    "revision": "d7771524124dea2614a7618a96e31fb2",
+    "url": "/myapp/static/media/computerImage.d7771524.jpeg"
+  },
+  {
+    "revision": "5beaa476afb2cd82c48e44d2b34757bd",
+    "url": "/myapp/static/media/design1.5beaa476.jpg"
+  },
+  {
+    "revision": "121c8138a484bf9f9663173a7c0163a8",
+    "url": "/myapp/static/media/india.121c8138.jpg"
+  },
+  {
+    "revision": "a551cf76c35f83887cc0a817e300cafa",
+    "url": "/myapp/static/media/insta.a551cf76.jpg"
+  },
+  {
+    "revision": "ec7d7ec7741ccc9a52494755272bdca8",
+    "url": "/myapp/static/media/leisure.ec7d7ec7.jpg"
+  },
+  {
+    "revision": "3c28d920d78e4939f3b2ecbc4f6dd973",
+    "url": "/myapp/static/media/pic1.3c28d920.jpg"
+  },
+  {
+    "revision": "e60e815287903c46c448655241259604",
+    "url": "/myapp/static/media/sea.e60e8152.jpg"
+  }
+]);
